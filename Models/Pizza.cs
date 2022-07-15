cce51f5da2a957223d920214f106c6a26a8cb316
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-07-15 16:40:11 +0200

    Completo (Da modificare il css)

--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,7 +1,13 @@
-﻿namespace la_mia_pizzeria_razor_layout
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_razor_layout.Models
 {
+    [Table("pizza")]
     public class Pizza
     {
+        [Key]
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
